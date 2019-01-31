dcad43e8fcbcb188916a7f1b06ed2d5131040e40
Jamin Pruitt <jaminpruitt@icloud.com>
2019-01-30 23:22:41 -0500

    added server

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const port = process.env.PORT || 3000;
+
+var app = express();
+
+
+app.use(express.static(__dirname + '/public'));
+
+
+app.listen(port, () => {
+    console.log(`server is up on port ${port}`);
+});
