edd3e8be743bf5209dd4523dc441e0e32b040a71
Jamin Pruitt <jaminpruitt@icloud.com>
2019-01-31 22:10:20 -0500

    test

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 3000;
 var app = express();
 
 
-app.use(express.static(__dirname + '/public/index.html'));
+app.use(express.static(__dirname + '/public/index'));
 
 
 app.listen(port, () => {
